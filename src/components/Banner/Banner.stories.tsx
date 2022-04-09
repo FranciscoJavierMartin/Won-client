ba82d0bc4586b821e22af8293b0e8424f6106fdf
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-04-09 20:24:09 +0200

    Create banner

---
+++ src/components/Banner/Banner.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, Story } from '@storybook/react';
+import Banner, { BannerProps } from '.';
+
+export default {
+  title: 'Banner',
+  component: Banner,
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+  },
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof Banner>;
+
+export const Default: Story<BannerProps> = (args) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Banner {...args} />
+  </div>
+);
+
+Default.args = {};
