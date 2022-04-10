29fa7e2169db4a303fc7412271d108995d2eb7ef
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-04-10 14:38:59 +0200

    Tests for promotional price

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -1,5 +1,6 @@
 import { renderWithTheme } from '@/utils/tests/helpers';
 import { screen } from '@testing-library/react';
+import theme from '../../styles/theme';
 import GameCard from '.';
 
 const props = {
@@ -29,5 +30,22 @@ describe('<GameCard />', () => {
     expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument();
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render price in label', () => {
+    renderWithTheme(<GameCard {...props} />);
+    const price = screen.getByText('99,99 €');
+    expect(price).not.toHaveStyle({ textDescription: 'line-through' });
+    expect(price).toHaveStyle({ backgroundColor: theme.colors.secondary });
+  });
+
+  it('should render a line-through in price when it is promotional', () => {
+    renderWithTheme(<GameCard {...props} promotionalPrice="79,99 €" />);
+    expect(screen.getByText('99,99 €')).toHaveStyle({
+      textDecoration: 'line-through',
+    });
+    expect(screen.getByText('79,99 €')).not.toHaveStyle({
+      textDecoration: 'line-through',
+    });
   });
 });
