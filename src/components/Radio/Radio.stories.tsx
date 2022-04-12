16f968aa333cbb07f29a73816b6fa9b652d5f4ea
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-04-12 21:19:08 +0200

    Create radio buttons

---
+++ src/components/Radio/Radio.stories.tsx
@@ -0,0 +1,55 @@
+import { ComponentMeta, Story } from '@storybook/react';
+import Radio, { RadioProps } from '.';
+
+export default {
+  title: 'Radio',
+  component: Radio,
+  args: {},
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark',
+    },
+  },
+  argTypes: {
+    onCheck: { action: 'checked' },
+  },
+} as ComponentMeta<typeof Radio>;
+
+export const Default: Story<RadioProps> = (args) => (
+  <>
+    <div style={{ padding: 10 }}>
+      <Radio
+        {...args}
+        label="First"
+        labelFor="first"
+        id="first"
+        name="name"
+        value="first"
+        defaultChecked
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Radio
+        {...args}
+        label="Second"
+        labelFor="second"
+        id="second"
+        name="name"
+        value="second"
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Radio
+        {...args}
+        label="Third"
+        labelFor="third"
+        id="third"
+        name="name"
+        value="third"
+      />
+    </div>
+  </>
+);
+
+Default.args = {};
