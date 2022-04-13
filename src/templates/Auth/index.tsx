5a9370cbb66a3ffda90cfe9da11816f3ce47a3ea
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-04-13 13:28:21 +0200

    Desktop styles

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -10,19 +10,25 @@ export type AuthProps = {
 const Auth: React.FC<AuthProps> = ({ title, children }) => (
   <S.Wrapper>
     <S.BannerBlock>
-      <Logo />
-      <Heading>All your favorite games in one place</Heading>
-      <S.Subtitle>
-        <strong>WON</strong> is the best and most complete gaming platform.
-      </S.Subtitle>
-      <S.Footer>Won Games 2022 &copy; All right reserved.</S.Footer>
+      <S.BannerContent>
+        <Logo />
+        <div>
+          <Heading>All your favorite games in one place</Heading>
+          <S.Subtitle>
+            <strong>WON</strong> is the best and most complete gaming platform.
+          </S.Subtitle>
+        </div>
+        <S.Footer>Won Games 2022 &copy; All right reserved.</S.Footer>
+      </S.BannerContent>
     </S.BannerBlock>
     <S.Content>
-      <Logo color="black" size="large" />
-      <Heading color="black" lineColor="secondary" lineLeft>
-        {title}
-      </Heading>
-      {children}
+      <S.ContentWrapper>
+        <Logo color="black" size="large" />
+        <Heading color="black" lineColor="secondary" lineLeft>
+          {title}
+        </Heading>
+        {children}
+      </S.ContentWrapper>
     </S.Content>
   </S.Wrapper>
 );
