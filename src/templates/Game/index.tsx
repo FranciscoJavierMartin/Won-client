381669f01ab2d66cec59b57639a3f7a05139acde
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-04-15 20:35:24 +0200

    Add Description

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,5 +1,6 @@
 import Gallery, { GalleryImageProps } from '@/components/Gallery';
 import GameInfo, { GameInfoProps } from '@/components/GameInfo';
+import TextContent from '@/components/TextContent';
 import Base from '@/templates/Base';
 import * as S from './styles';
 
@@ -7,9 +8,15 @@ export type GameProps = {
   cover: string;
   gameInfo: GameInfoProps;
   gallery?: GalleryImageProps[];
+  description: string;
 };
 
-const Game: React.FC<GameProps> = ({ cover, gameInfo, gallery }) => (
+const Game: React.FC<GameProps> = ({
+  cover,
+  gameInfo,
+  gallery,
+  description,
+}) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
     <S.Main>
@@ -19,6 +26,9 @@ const Game: React.FC<GameProps> = ({ cover, gameInfo, gallery }) => (
       <S.SectionGallery>
         {!!gallery && <Gallery items={gallery} />}
       </S.SectionGallery>
+      <S.SectionDescription>
+        <TextContent title="Descrition" content={description} />
+      </S.SectionDescription>
     </S.Main>
   </Base>
 );
