7b3cb7614440927b1e691b9cb29f210ab8a29f33
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-04-15 20:00:07 +0200

    Add game info

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,13 +1,20 @@
+import GameInfo, { GameInfoProps } from '@/components/GameInfo';
 import Base from '@/templates/Base';
 import * as S from './styles';
 
 export type GameProps = {
   cover: string;
+  gameInfo: GameInfoProps;
 };
 
-const Game: React.FC<GameProps> = ({ cover }) => (
+const Game: React.FC<GameProps> = ({ cover, gameInfo }) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
+    <S.Main>
+      <S.SectionGameInfo>
+        <GameInfo {...gameInfo} />
+      </S.SectionGameInfo>
+    </S.Main>
   </Base>
 );
 
