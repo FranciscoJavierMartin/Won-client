7d9b50d286a4fa60522b3cd5c6ce2a12a09ba8db
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-04-15 19:30:02 +0200

    Add static generations

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -1,5 +1,29 @@
-import Game from '@/templates/Game';
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next';
+import { ParsedUrlQuery } from 'querystring';
+import Game, { GameProps } from '@/templates/Game';
 
-export default function Index() {
-  return <Game />;
+export default function Index(
+  props: InferGetStaticPropsType<typeof getStaticProps>
+) {
+  return <Game {...props} />;
 }
+
+export const getStaticProps: GetStaticProps<GameProps> = async () => {
+  return {
+    props: {
+      cover:
+        'https://images.gog-statics.com/5643a7c831df452d29005caeca24c28cdbfaa6fbea5a9556b147ee26d325fa70_bg_crop_1366x655.jpg',
+    },
+  };
+};
+
+interface IParams extends ParsedUrlQuery {
+  slug: string;
+}
+
+export const getStaticPaths: GetStaticPaths<IParams> = () => {
+  return {
+    paths: [{ params: { slug: 'test' } }],
+    fallback: false,
+  };
+};
