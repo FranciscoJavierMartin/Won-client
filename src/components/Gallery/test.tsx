cebc7818762ad951bb53c720162a4173ff576f89
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-04-15 10:08:49 +0200

    Close modal

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -33,4 +33,18 @@ describe('<Gallery />', () => {
     expect(modal.getAttribute('aria-hidden')).toBe('false');
     expect(modal).toHaveStyle({ opacity: 1 });
   });
+
+  it('should handle close modal when overlay or click on button', () => {
+    renderWithTheme(<Gallery items={items.slice(0, 2)} />);
+    const modal = screen.getByLabelText('modal');
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    );
+
+    fireEvent.click(screen.getByRole('button', { name: /Close modal/i }));
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true');
+    expect(modal).toHaveStyle({ opacity: 0, pointerEvents: 'none' });
+  });
 });
