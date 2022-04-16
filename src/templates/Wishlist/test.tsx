60f9fece014272b520e20fb111bc7f176966372c
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-04-16 12:33:17 +0200

    Add test

--- src/templates/Wishlist/test.tsx
+++ src/templates/Wishlist/test.tsx
@@ -1,14 +1,24 @@
 import { screen } from '@testing-library/react';
 import { renderWithTheme } from '@/utils/tests/helpers';
 import Wishlist from '.';
+import gamesMock from '@/components/GameCardSlider/mock';
+import highlightMock from '@/components/Highlight/mock';
+
+const props = {
+  recommendedGames: gamesMock,
+  recommendedHighlight: highlightMock,
+};
+
+jest.mock('@/components/Showcase', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Showcase" />;
+  },
+}));
 
 describe('<Wishlist />', () => {
-  xit('should render the heading', () => {
-    const { container } = renderWithTheme(<Wishlist />);
-
-    expect(
-      screen.getByRole('heading', { name: /Wishlist/i })
-    ).toBeInTheDocument();
-    expect(container.firstChild).toMatchSnapshot();
+  it('should render the heading', () => {
+    renderWithTheme(<Wishlist {...props} />);
+    expect(screen.getByTestId('Mock Showcase')).toBeInTheDocument();
   });
 });
