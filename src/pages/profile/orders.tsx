37d237bf128d7245e873c99731db1da060c3f3cc
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-04-18 21:27:04 +0200

    Add orderslist to page

--- src/pages/profile/orders.tsx
+++ src/pages/profile/orders.tsx
@@ -1,3 +1,24 @@
-export default function Index() {
-  return <div></div>;
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import Profile from '@/templates/Profile';
+import itemsMock from '@/components/OrdersList/mock';
+import OrdersList, { OrdersListProps } from '@/components/OrdersList';
+
+export default function Index(
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
+  return (
+    <Profile>
+      <OrdersList {...props} />
+    </Profile>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps<
+  OrdersListProps
+> = async () => {
+  return {
+    props: {
+      items: itemsMock,
+    },
+  };
+};
