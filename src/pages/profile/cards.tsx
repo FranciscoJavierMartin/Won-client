90bf33bb32e9086c4ce90cef63fe26528a76df89
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-04-18 20:49:13 +0200

    Add cards list to page

--- src/pages/profile/cards.tsx
+++ src/pages/profile/cards.tsx
@@ -1,3 +1,24 @@
-export default function Index() {
-  return <div></div>;
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import Profile from '@/templates/Profile';
+import CardsList, { CardsListProps } from '@/components/CardsList';
+import cardsMock from '@/components/PaymentOptions/mock';
+
+export default function Index(
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
+  return (
+    <Profile>
+      <CardsList {...props} />
+    </Profile>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps<
+  CardsListProps
+> = async () => {
+  return {
+    props: {
+      cards: cardsMock,
+    },
+  };
+};
