9a6de137cdf26e1753c42c9f16e4f678735c27eb
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-04-19 22:41:10 +0200

    Fix test

--- src/components/Dropdown/test.tsx
+++ src/components/Dropdown/test.tsx
@@ -18,13 +18,13 @@ describe('<Dropdown />', () => {
     expect(screen.getByLabelText(/toggle dropdown/)).toBeInTheDocument();
   });
 
-  xit('should handle open/close dropdown', () => {
+  it('should handle open/close dropdown', async () => {
     const content = screen.getByText(/content/).parentElement!;
 
     expect(content).toHaveStyle({ opacity: 0 });
     expect(content.getAttribute('aria-hidden')).toBe('true');
 
-    userEvent.click(screen.getByLabelText(/toggle dropdown/));
+    await userEvent.click(screen.getByLabelText(/toggle dropdown/));
 
     expect(content).toHaveStyle({ opacity: 1 });
     expect(content.getAttribute('aria-hidden')).toBe('false');
