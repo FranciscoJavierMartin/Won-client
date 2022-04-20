5e5a3218700c5f8fd76042f9c844e27728940cea
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-04-20 22:39:49 +0200

    Connect client throguht graphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,33 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import theme from '../styles/theme';
 import GlobalStyles from '../styles/global';
 
 function App({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="Game Store to get the lastest releases"
-        />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won Games</title>
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="Game Store to get the lastest releases"
+          />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,31 @@
 import type { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { gql, useQuery } from '@apollo/client';
 import Home, { HomeProps } from '@/templates/Home';
 import bannersMock from '@/components/BannerSlider/mock';
 import gamesMock from '@/components/GameCardSlider/mock';
 import highlightMock from '@/components/Highlight/mock';
+
+export default function Index(
+  props: InferGetStaticPropsType<typeof getStaticProps>
+) {
+  const { data, loading, error } = useQuery(gql`
+    query getGames {
+      games {
+        data {
+          attributes {
+            name
+          }
+        }
+      }
+    }
+  `);
+
+  if (loading) return <p>Loading</p>;
+  if (error) return <p>{error}</p>;
+  if (data) return <p>{JSON.stringify(data, null, 2)}</p>;
+
+  return <Home {...props} />;
+}
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   return {
@@ -19,9 +42,3 @@ export const getStaticProps: GetStaticProps<HomeProps> = async () => {
     },
   };
 };
-
-export default function Index(
-  props: InferGetStaticPropsType<typeof getStaticProps>
-) {
-  return <Home {...props} />;
-}
